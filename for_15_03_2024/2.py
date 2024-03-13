6a130df21eea7d0ea0bc56227d539bdb3738e3f3
b3h4re <lev.makarofff@gmail.com>
2024-03-13 18:01:09 +0300

    2nd task for 15.03.2024

--- for_15_03_2024/2.py
+++ for_15_03_2024/2.py
@@ -2,36 +2,52 @@ from graph import Graph
 from collections import deque
 
 
-def find_paths(graph, path_exists):
-    for start in range(len(graph)):
-        q = deque()
-        q.append(start)
-        path_exists[start].add(start)
-        while len(q) > 0:
-            vertex = q.popleft()
-            for neighbor in graph[vertex][0]:
-                if neighbor not in path_exists[start]:
-                    path_exists[start].add(neighbor)
-                    q.append(neighbor)
+def find_paths(graph, start, comp):
+    q = deque()
+    q.append(start)
+    comp.add(start)
+    while len(q) > 0:
+        vertex = q.popleft()
+        for neighbor, _ in graph[vertex]:
+            if neighbor not in comp:
+                comp.add(neighbor)
+                q.append(neighbor)
+
+
+def calculate_total_weight(graph, component):
+    res = 0
+    added = set()
+    visited = set()
+    for vertex in component:
+        if vertex not in visited:
+            visited.add(vertex)
+            for neighbor, weight in graph[vertex]:
+                if (vertex, neighbor) not in added:
+                    added.add((vertex, neighbor))
+                    added.add((neighbor, vertex))
+                    res += weight
+    return res
 
 
 f = open("inputs/2.txt")
 n, m = map(int, f.readline().split())
-g = Graph(n, oriented=True, weighed=True)
+g = Graph(n, oriented=False, weighed=True)
 for _ in range(m):
     start, end, weight = map(int, f.readline().split())
     g.add_connection(start, end, weight)
 f.close()
 
-path_exists = [set() for _ in range(n)]
-find_paths(g, path_exists)
-
 components = []
-included_vertices = [False] * n
-for v in range(n):
+used = set()
+for start in range(n):
     component = set()
-    included_vertices[v] = True
-    component.add(v)
-    for connected in path_exists[v]:
-        if v in path_exists[connected]:
-            component.add(connected)
+    if start not in used:
+        find_paths(g, start, component)
+        used.update(component)
+        components.append(component)
+
+ans = []
+for component in components:
+    ans.append(calculate_total_weight(g, component))
+ans.sort()
+print(*ans, sep='\n')
