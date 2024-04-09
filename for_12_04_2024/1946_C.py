8a1ac047650b8eef2d8b4b8c0d190f3b23353065
b3h4re <lev.makarofff@gmail.com>
2024-04-09 21:32:33 +0300

    better approach

--- for_12_04_2024/1946_C.py
+++ for_12_04_2024/1946_C.py
@@ -12,31 +12,42 @@ class Graph:
         if not self._oriented:
             self._core[end].add((start, weight) if self._weighed else start)
 
+    def remove_connection(self, start, end):
+        self._core[start].discard(end)
+        if not self._oriented:
+            self._core[end].remove(start)
+
     def __getitem__(self, item):
         return self._core[item]
 
 
-def dfs(graph, start, visited, subtree):
-    visited.add(start)
+def find_start(graph):
+    for i in range(len(graph)):
+        if len(graph[i]) == 1:
+            return i
+
+
+def dfs(graph, start, subtree, last, x, component):
+    if subtree > x:
+        subtree = 1
+        component[0] += 1
+
     for vertex in graph[start]:
-        if vertex not in visited:
-            dfs(graph, vertex, visited, subtree+1)
+        if vertex != last:
+            dfs(graph, vertex, subtree+1, start, x, component)
 
 
 def is_cuttable(graph, x, k):
-    start = 0
-    while len(graph[start]) > 1:
-        start += 1
-    visited = set()
-    while len(visited) < len(graph):
-        dfs(graph, start, visited, set())
+    components = [1]
+    dfs(graph, find_start(graph), 1, -1, x, components)
+    return components[0] > k
 
 
 def get_ans(graph, k):
     l, r = 1, len(graph) + 1
     while l + 1 != r:
         m = (l + r) // 2
-        if is_cuttable(graph, m):
+        if is_cuttable(graph, m, k):
             l = m
         else:
             r = m
@@ -50,5 +61,7 @@ for _ in range(t):
     g = Graph(n=n, oriented=False, weighed=False)
     for __ in range(n-1):
         u, v = map(int, input().split())
-        g.add_connection(u, v)
+        g.add_connection(u-1, v-1)
+    if _ == 2:
+        pass
     print(get_ans(g, k))
