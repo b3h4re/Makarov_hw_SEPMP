0c3a6e17eefd31b982b7e60ddce52dedfb3eabbd
b3h4re <lev.makarofff@gmail.com>
2024-04-10 00:51:37 +0300

    accepted

--- for_12_04_2024/1946_C.py
+++ for_12_04_2024/1946_C.py
@@ -1,67 +1,68 @@
-class Graph:
-    def __init__(self, n, oriented=False, weighed=False):
-        self._oriented = oriented
-        self._weighed = weighed
-        self._core = [set() for _ in range(n)]
-
-    def __len__(self):
-        return len(self._core)
-
-    def add_connection(self, start, end, weight=0):
-        self._core[start].add((end, weight) if self._weighed else end)
-        if not self._oriented:
-            self._core[end].add((start, weight) if self._weighed else start)
-
-    def remove_connection(self, start, end):
-        self._core[start].discard(end)
-        if not self._oriented:
-            self._core[end].remove(start)
-
-    def __getitem__(self, item):
-        return self._core[item]
+from collections import deque
 
 
-def find_start(graph):
-    for i in range(len(graph)):
-        if len(graph[i]) == 1:
-            return i
+def add_connection(graph, start, end):
+    graph[start].add(end)
+    graph[end].add(start)
 
 
-def dfs(graph, start, subtree, last, x, component):
-    if subtree > x:
-        subtree = 1
-        component[0] += 1
+def say_no_to_recursion(graph, start, last, x):
+    q = deque()
+    q.append((start, last))
+    stack = []
 
-    for vertex in graph[start]:
-        if vertex != last:
-            dfs(graph, vertex, subtree+1, start, x, component)
+    while len(q) > 0:
+        vertex, parent = q.popleft()
+
+        for neighbor in graph[vertex]:
+            if neighbor != parent:
+                stack.append((neighbor, vertex))
+                q.append((neighbor, vertex))
+
+    res = 1
+    subtree = [1] * n
+    while len(stack) > 0:
+        vertex, parent = stack.pop()
+        if subtree[vertex] >= x:
+            subtree[vertex] = 0
+            res += 1
+
+        subtree[parent] += subtree[vertex]
+
+    return subtree[start], res
 
 
 def is_cuttable(graph, x, k):
-    components = [1]
-    dfs(graph, find_start(graph), 1, -1, x, components)
-    return components[0] > k
+    start = 0
+    while len(graph[start]) != 1:
+        start += 1
+    tmp, components = say_no_to_recursion(graph, start, start, x)
+    if components > k + 1:
+        return True
+    if components == k + 1:
+        return tmp >= x
+    return False
 
 
 def get_ans(graph, k):
     l, r = 1, len(graph) + 1
     while l + 1 != r:
         m = (l + r) // 2
-        if is_cuttable(graph, m, k):
+        if is_cuttable(g, m, k):
             l = m
         else:
             r = m
     return l
 
 
-t = int(input())
+if __name__ == '__main__':
+    t = int(input())
 
-for _ in range(t):
-    n, k = map(int, input().split())
-    g = Graph(n=n, oriented=False, weighed=False)
-    for __ in range(n-1):
-        u, v = map(int, input().split())
-        g.add_connection(u-1, v-1)
-    if _ == 2:
-        pass
-    print(get_ans(g, k))
+    for test in range(1, t+1):
+        n, k = map(int, input().split())
+        g = [set() for __ in range(n)]
+        for __ in range(n-1):
+            u, v = map(int, input().split())
+            add_connection(g, u-1, v-1)
+
+        print(get_ans(g, k))
