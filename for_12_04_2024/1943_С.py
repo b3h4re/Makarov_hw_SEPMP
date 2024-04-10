5289ae4fd171672723c758a83ba776ee3ea02583
b3h4re <lev.makarofff@gmail.com>
2024-04-10 10:13:51 +0300

    codeforces problems

--- for_12_04_2024/1943_С.py
+++ for_12_04_2024/1943_С.py
@@ -1,12 +1,51 @@
-from graph import Graph
+from collections import deque
 
 
-t = int(input())
+def add_connection(graph, start, end):
+    graph[start].add(end)
+    graph[end].add(start)
 
-for _ in range(t):
-    n = int(input())
-    g = Graph(n=n, oriented=False, weighed=False)
-    for __ in range(n-1):
-        u, v = map(int, input().split())
-        g.add_connection(u, v)
-    
+
+def bfs(graph, start, coloring):
+    q = deque()
+    colored = set()
+    q.append((start, 0, start))
+    coloring.append((start, 1))
+
+    max_dist = -1
+    while len(q) > 0:
+        vertex, dist, parent = q.popleft()
+
+
+        for neighbor in graph[vertex]:
+            if neighbor != parent:
+                if dist + 1 >= min_dist:
+                    return min_dist + 1
+                q.append((neighbor, dist+1, vertex))
+    return max_dist
+
+
+def get_ans(graph):
+    if len(graph) == 1:
+        print("1\n1 0")
+        return
+    vertices = [(i, len(graph[i])) for i in range(len(graph))]
+    vertices.sort(key=(lambda x: x[1]), reverse=True)
+    start = vertices[0][0]
+    coloring = []
+    bfs(graph, start, coloring)
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for test in range(1, t+1):
+        n = int(input())
+
+        g = [set() for __ in range(n)]
+        for __ in range(n-1):
+            u, v = map(int, input().split())
+            add_connection(g, u-1, v-1)
+
+        get_ans(g)
--- for_12_04_2024/1944_A.py
+++ for_12_04_2024/1944_A.py
@@ -1,10 +1,27 @@
-def get_ans(bridges, can_destroy):
+def get_ans(vertices, can_destroy):
+    if vertices == 1:
+        return 1
     if can_destroy == 0:
-        return bridges
+        return vertices
+
+    res = vertices
+
+    rem = 0
+    rem_max = 0
+
+    for x in range(vertices-1, 0, -1):
+        rem += x - (vertices - x - 1)
+        rem_max += x
+
+        if rem <= can_destroy <= rem_max:
+            res = x
+
+    return res
 
 
-t = int(input())
+if __name__ == '__main__':
+    t = int(input())
 
-for _ in range(t):
-    n, k = map(int, input().split())
-    print(get_ans(n, k))
+    for test in range(1, t+1):
+        n, k = map(int, input().split())
+        print(get_ans(n, k))
