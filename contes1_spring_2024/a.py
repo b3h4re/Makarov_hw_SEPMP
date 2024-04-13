1be2f5180dc431d074161507f5190bcafecb6478
b3h4re <lev.makarofff@gmail.com>
2024-04-13 17:42:29 +0300

    trying

--- contes1_spring_2024/a.py
+++ contes1_spring_2024/a.py
@@ -1,20 +1,49 @@
-from heapq import heapify, heappop, heappush
+from collections import deque
+from heapq import heappush
+
+
+def get_parent(x):
+    if x % 2 == 1:
+        return (x - 1) // 2
+    return (x - 2) // 2
+
+
+def calc_bigger(heap, elem):
+    res = 0
+    q = deque()
+    used = set()
+    for i in range(len(heap)-1, -1, -1):
+        if 2*i + 1 < len(heap):
+            break
+        parent = get_parent(i)
+        if elem < heap[i]:
+            res += 1
+            if parent >= 0 and parent not in used:
+                q.append(parent)
+                used.add(parent)
+
+    while len(q) > 0:
+        v = q.popleft()
+        parent = get_parent(v)
+        if elem < heap[v]:
+            res += 1
+            if parent >= 0 and parent not in used:
+                q.append(parent)
+                used.add(parent)
+    return res
 
 
 t = int(input())
 
-for _ in range(t):
+for test in range(1, t+1):
     n = int(input())
     ladder = list(map(int, input().split()))
-    heap = [ladder[0]]
-    heapify(heap)
-    res = 0
-    for i in range(1, n):
-        removed = []
-        elem = 0
 
-        while elem < ladder[i] and len(heap) > 0:
-            elem = heappop(heap)
-            removed.append(elem)
-            res += elem
-            
+    heap = [ladder[-1]]
+    res = 0
+    for i in range(n-2, -1, -1):
+        bigger = calc_bigger(heap, ladder[i])
+        res += ladder[i] * bigger
+        heappush(heap, ladder[i])
+
+    print(res)
--- contes1_spring_2024/e.py
+++ contes1_spring_2024/e.py
@@ -1,1 +1,55 @@
+def add_connection(graph, start, end):
+    graph[start-1].add(end-1)
+    graph[end-1].add(start-1)
+
+
+def dfs(graph, start, last, color, edges, cycles, ans):
+    color[start] = 1
+    ok = True
+    for vertex in graph[start]:
+        if vertex == last:
+            continue
+        if color[vertex] == 0:
+            edges.append((start, vertex))
+            dfs(graph, vertex, start, color, edges, cycles,ans)
+            edges.pop()
+        elif color[vertex] == 1:
+            ok = not cycles[start][vertex]
+            cycles[start][vertex], cycles[vertex][start] = True, True
+            length = 2
+            for i in range(len(edges)-1, -1, -1):
+                s, e = edges[i]
+                if e == vertex:
+                    break
+                ok *= not cycles[s][e]
+                cycles[s][e], cycles[e][s] = True, True
+                length += 1
+            ans.append(length)
+        if not ok:
+            return
+    color[start] = 2
+
+
 n, m = map(int, input().split())
+g = [set() for _ in range(n)]
+
+for _ in range(m):
+    ki, *path = map(int, input().split())
+    for i in range(1, ki):
+        add_connection(g, path[i-1], path[i])
+
+colors = [0] * n
+in_cycle = [[False for _ in range(n)] for __ in range(n)]
+edges = []
+cycle_lengths = []
+
+dfs(g, 0, -1, colors, edges, in_cycle, cycle_lengths)
+for i in range(n):
+    if colors[i] != 2:
+        print(0)
+        break
+else:
+    ans = 1
+    for elem in cycle_lengths:
+        ans *= elem
+    print(ans)
