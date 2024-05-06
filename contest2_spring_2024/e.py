8708c905dc2dee47c3f4e1605e06207ffb0b84cf
b3h4re <lev.makarofff@gmail.com>
2024-05-06 10:56:54 +0300

    eeeeeeeeeeeee

--- contest2_spring_2024/e.py
+++ contest2_spring_2024/e.py
@@ -1,34 +1,76 @@
-def dfs(graph, flows, used, start, end, flow, num):
-    if start == end:
-        return flow
-    used[start] = num
-    for vertex, edge in graph[start]:
-        if used[vertex] != num and edge - flows[start][vertex] > 0:
-            delta = dfs(graph, flows, used, vertex, end, min(flow, edge - flows[start][vertex]), num)
-            if delta > 0:
-                flows[start][vertex] += delta
-                flows[vertex][start] -= delta
-                return delta
-    return 0
+from collections import deque
+
+
+# def dfs(graph, flows, used, start, end, flow, num):
+#     if start == end:
+#         return flow
+#     used[start] = num
+#     for vertex, edge in graph[start]:
+#         if used[vertex] != num and edge - flows[start][vertex] > 0:
+#             delta = dfs(graph, flows, used, vertex, end, min(flow, edge - flows[start][vertex]), num)
+#             if delta > 0:
+#                 flows[start][vertex] += delta
+#                 flows[vertex][start] -= delta
+#                 return delta
+#     return 0
+
+
+def bfs(graph, start, end, parent, used, flows):
+    q = deque()
+    q.append(start)
+    used[start] = True
+    while len(q) > 0:
+        vertex = q.popleft()
+        if vertex == end:
+            return
+        for neighbor in range(len(graph)):
+            if flows[vertex][neighbor] > 0 and not used[neighbor]:
+                used[neighbor] = True
+                parent[neighbor] = vertex
+                q.append(neighbor)
+
+
+def get_flow(end, parent, flows):
+    if parent[end] == -1:
+        return
+    flow = get_flow(parent[end], parent, flows)
+    if flow is None:
+        return flows[parent[end]][end]
+    return min(flows[parent[end]][end], get_flow(parent[end], parent, flows))
+
+
+def adjust_flows(end, flow, flows, parent):
+    if parent[end] == -1:
+        return
+    adjust_flows(parent[end], flow, flows, parent)
+    flows[parent[end]][end] -= flow
+    flows[end][parent[end]] += flow
 
 
 def find_flow(graph, start,  end):
-    used = [0] * len(graph)
-    flows = [[0 for _ in range(len(graph))] for __ in range(len(graph))]
-    num = 1
-    x, res = 1, 0
-    while x > 0:
-        x = dfs(graph, flows, used, start, end, int(1e12), num)
-        res += x
-        num += 1
+    res = 0
+    flows = [[graph[j][i] for i in range(len(graph))] for j in range(len(graph))]
+    while True:
+        used = [False] * len(graph)
+        parent = [-1] * len(graph)
+        bfs(graph, start, end, parent, used, flows)
+
+        flow = get_flow(end, parent, flows)
+        adjust_flows(end, flow, flows, parent)
+
+        if flow is None:
+            break
+        res += flow
     return res
 
 
 def process_test(graph):
-    mn = int(1e12)
+    mn = None
     for v in range(1, len(graph)):
         flow = find_flow(graph, 0, v)
-        mn = min(flow, mn)
+        if mn is None:
+            mn = flow
+        mn = min(mn, flow)
     return mn
 
 
@@ -38,14 +80,12 @@ if __name__ == "__main__":
     for test in range(1, t+1):
         n = int(input())
 
-        cities = [set() for _ in range(n)]
+        cities = [[0 for __ in range(n)] for _ in range(n)]
         for i in range(n):
             line = list(map(int, input()))
             for j in range(n):
                 if line[j] > 0:
-                    cities[i].add(
-                        (j, line[j])
-                    )
+                    cities[i][j] = line[j]
         ans.append(process_test(cities))
 
     for elem in ans:
